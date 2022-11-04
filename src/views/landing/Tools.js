5c19a983c8c755733130e0e6b8aed22f7050b65c
Giancarlo Sanchez <gianksp@gmail.com>
2022-11-04 10:48:29 +0000

    Support multiple chains EVM

--- src/views/landing/Tools.js
+++ src/views/landing/Tools.js
@@ -1,39 +1,66 @@
+import { useEffect, useState } from "react";
 // material-ui
 import { Container, Grid, Typography, Box } from "@mui/material";
 // project imports
 import { gridSpacing } from "store/constant";
 // assets
 import { constants } from "react-dappify";
+import axios from "axios";
 
 const { NETWORKS, LOGO } = constants;
 
 // ============================|| LANDING - KEY FEATURE PAGE ||============================ //
 
 const ToolsPage = () => {
+	const [networks, setNetworks] = useState([]);
+
+	const loadSupportedNetworks = async () => {
+		const response = await axios.get(
+			`${process.env.REACT_APP_DAPPIFY_API_URL}/chain?type=mainnet`,
+			{
+				headers: {
+					"x-api-Key": process.env.REACT_APP_DAPPIFY_API_KEY,
+					accept: "application/json"
+				}
+			}
+		);
+		setNetworks(response.data);
+	};
+
+	useEffect(() => {
+		loadSupportedNetworks();
+	}, []);
+
 	const displayChains = () => {
 		const chains = [];
-		const supportedNetworks = Object.keys(NETWORKS);
-		supportedNetworks.forEach((chain) => {
-			const network = NETWORKS[chain];
-			const logo = LOGO[network.nativeCurrency.symbol];
+
+		// const supportedNetworks = Object.keys(NETWORKS);
+		networks?.forEach((network) => {
+			// const network = NETWORKS[chain];
+			// const logo = LOGO[network.nativeCurrency.symbol];
+			const name = network.name.replace("Mainnet", "");
 			chains.push(
-				<Grid item xs={6} md={2} key={network.chainName} sx={{ my: 2 }}>
+				<Grid item xs={2} key={name} sx={{ my: 2 }}>
 					<Box>
 						<Grid item xs={12}>
 							<img
-								src={logo}
-								alt={network.chainName}
-								height="96"
+								src={network.imageUrl}
+								alt={name}
+								height="36"
 								style={{
-									maxHeight: "96px",
-									margin: "0 auto"
+									maxHeight: "36px",
+									margin: "0 auto",
+									borderRadius: "50%"
+								}}
+								onError={(event) => {
+									event.target.src =
+										"https://chainlist.org/_next/image?url=%2Funknown-logo.png&w=64&q=75";
+									event.onerror = null;
 								}}
 							/>
 						</Grid>
 						<Grid item xs={12} sx={{ py: 2 }}>
-							<Typography variant="h5">
-								{network.chainName}
-							</Typography>
+							<Typography variant="h5">{name}</Typography>
 						</Grid>
 					</Box>
 				</Grid>
--- src/views/new/Blockchain.js
+++ src/views/new/Blockchain.js
@@ -1,60 +1,103 @@
 /* eslint-disable react/no-unescaped-entities */
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
 import {
 	Grid,
 	Typography,
 	Tooltip,
 	FormControl,
-	InputLabel,
-	MenuItem,
-	Select,
-	Box
+	// InputLabel,
+	// MenuItem,
+	// Select,
+	Box,
+	Autocomplete,
+	TextField,
+	Switch,
+	FormControlLabel,
+	FormGroup
 } from "@mui/material";
-import { constants } from "react-dappify";
+// import { constants } from "react-dappify";
+import axios from "axios";
 
-const { NETWORKS, LOGO } = constants;
+// const { NETWORKS, LOGO } = constants;
 
 const Blockchain = ({ onChange }) => {
-	const [selectedChain, setSelectedChain] = useState();
+	// const [selectedChain, setSelectedChain] = useState();
+
+	const [networks, setNetworks] = useState([]);
+	const [filter, setFilter] = useState("mainnet");
+
+	const loadSupportedNetworks = async () => {
+		const response = await axios.get(
+			`${process.env.REACT_APP_DAPPIFY_API_URL}/chain?type=${filter}`,
+			{
+				headers: {
+					"x-api-Key": process.env.REACT_APP_DAPPIFY_API_KEY,
+					accept: "application/json"
+				}
+			}
+		);
+		setNetworks(response.data);
+	};
+
+	useEffect(() => {
+		loadSupportedNetworks();
+	}, [filter]);
 
 	const projectInfo = `
     Dappify supports all EVMs. Having said this, each blockchain has a specific set of traits that make it more appealing depending on your specific needs. Have a read at each of the offering descriptions for more information
     `;
 
-	const renderMenuItems = () => {
-		const list = [];
-		const supportedNetworks = Object.keys(NETWORKS);
-		supportedNetworks.forEach((chainId) => {
-			const targetNetwork = NETWORKS[chainId];
-			list.push(
-				<MenuItem value={chainId} key={chainId}>
-					<Grid container>
-						<Box sx={{ minWidth: 30 }}>
-							<img
-								src={LOGO[targetNetwork.nativeCurrency.symbol]}
-								alt="Ava"
-								height="20px"
-							/>
-						</Box>
-						<Typography
-							variant="h3"
-							fontWeight={200}
-							sx={{ ml: 2 }}
-						>
-							{targetNetwork.chainName}
-						</Typography>
-					</Grid>
-				</MenuItem>
-			);
-		});
-		return list;
-	};
+	// const renderMenuItems = () => {
+	// 	const list = [];
+	// 	// const supportedNetworks = Object.keys(NETWORKS);
+	// 	networks?.forEach((targetNetwork) => {
+	// 		// const targetNetwork = NETWORKS[chainId];
+	// 		const name = targetNetwork.name;
+	// 		list.push(
+	// 			<MenuItem value={targetNetwork.chainId} key={targetNetwork.chainId}>
+	// 				<Grid container>
+	// 					<Box sx={{ minWidth: 30 }}>
+	// 					<img
+	// 						src={targetNetwork.imageUrl}
+	// 						alt={name}
+	// 						height="24"
+	// 						style={{
+	// 							maxHeight: "24px",
+	// 							margin: "0 auto",
+	// 							borderRadius: '50%'
+	// 						}}
+	// 						onError={(event) => {
+	// 							event.target.src = "https://chainlist.org/_next/image?url=%2Funknown-logo.png&w=64&q=75"
+	// 							event.onerror = null
+	// 						}}
+	// 					/>
+	// 					</Box>
+	// 					<Typography
+	// 						variant="h3"
+	// 						fontWeight={200}
+	// 						sx={{ ml: 2 }}
+	// 					>
+	// 						{targetNetwork.name}
+	// 					</Typography>
+	// 				</Grid>
+	// 			</MenuItem>
+	// 		);
+	// 	});
+	// 	return list;
+	// };
 
-	const handleChange = (event) => {
-		const newChainId = event.target.value;
-		setSelectedChain(newChainId);
-		onChange(newChainId);
+	// const handleChange = (event) => {
+	// 	const newChainId = event.target.value;
+	// 	setSelectedChain(newChainId);
+	// 	console.log(newChainId);
+	// 	onChange(newChainId);
+	// };
+
+	const handleSelectNetwork = (e, value) => {
+		console.log(e.target, value);
+		// setSelectedChain(value.chainId);
+		onChange(value.chainId);
 	};
 
 	return (
@@ -81,10 +124,7 @@ const Blockchain = ({ onChange }) => {
 			<Grid container spacing={1}>
 				<Grid item xs={12}>
 					<FormControl fullWidth>
-						<InputLabel id="network-select-label">
-							Select your ecosystem
-						</InputLabel>
-						<Select
+						{/* <Select
 							labelId="network-select-label"
 							id="network-select"
 							label="Select your ecosystem"
@@ -92,7 +132,77 @@ const Blockchain = ({ onChange }) => {
 							onChange={handleChange}
 						>
 							{renderMenuItems()}
-						</Select>
+	</Select> */}
+
+						<FormGroup>
+							<FormControlLabel
+								control={
+									<Switch
+										checked={filter === "testnet"}
+										onChange={() => {
+											setFilter(
+												filter === "testnet"
+													? "mainnet"
+													: "testnet"
+											);
+										}}
+									/>
+								}
+								label="Testnets"
+							/>
+						</FormGroup>
+						<Autocomplete
+							id="country-select-demo"
+							fullWidth
+							options={networks}
+							autoHighlight
+							getOptionLabel={(option) => option.name}
+							onChange={handleSelectNetwork}
+							renderOption={(props, option) => (
+								<li {...props}>
+									<Grid
+										container
+										sx={{ px: 3, py: 1, cursor: "pointer" }}
+										key={option.chainId}
+									>
+										<Box sx={{ minWidth: 30 }}>
+											<img
+												src={option.imageUrl}
+												alt={option.name}
+												height="24"
+												style={{
+													maxHeight: "24px",
+													margin: "0 auto",
+													borderRadius: "50%"
+												}}
+												onError={(event) => {
+													event.target.src =
+														"https://chainlist.org/_next/image?url=%2Funknown-logo.png&w=64&q=75";
+													event.onerror = null;
+												}}
+											/>
+										</Box>
+										<Typography
+											variant="h3"
+											fontWeight={200}
+											sx={{ ml: 2 }}
+										>
+											{option.name}
+										</Typography>
+									</Grid>
+								</li>
+							)}
+							renderInput={(params) => (
+								<TextField
+									{...params}
+									label="Select your ecosystem"
+									inputProps={{
+										...params.inputProps,
+										autoComplete: "new-password" // disable autocomplete and autofill
+									}}
+								/>
+							)}
+						/>
 					</FormControl>
 				</Grid>
 			</Grid>
